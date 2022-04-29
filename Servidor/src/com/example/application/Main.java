32aabbe9a75239d71d3e001fc79840e25a233cf9
FrancescBagueMarti <francesc.bague@gmail.com>
2022-04-29 16:09:59 +0200

    Subida de proyecto

---
+++ Servidor/src/com/example/application/Main.java
@@ -0,0 +1,16 @@
+package com.example.application;
+
+/**
+ *
+ * @author francesc
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
